5b5725327ec9d4ed3bdd648cb518c561e8d25b76
dmquinn <danielmurphyberling@gmail.com>
2022-02-24 22:01:57 +0100

    test vscode config

--- backend/controllers/artistController.js
+++ backend/controllers/artistController.js
@@ -62,7 +62,7 @@ const updateArtist = asyncHandler(async (req, res) => {
     throw new Error("Artist not found");
   }
 });
-
+//test push command
 module.exports = {
   getArtists,
   getArtistById,
